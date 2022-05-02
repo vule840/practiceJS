4f6b7b616b34122024c6b4e40754585bc1329f7a
Tomislav Vukusic <tomislav.vukusic@distrelec.com>
2022-05-02 19:54:24 +0200

    merge 2 objects in array of objects

--- practiceJS.js
+++ practiceJS.js
@@ -24,23 +24,19 @@ let PRODUCTS = [
   let ITEMS_WITH_WARRANTY = [
     {
       sku: 16901348,
-      items: [1,2,3]
+      warranty: 1,
     },
     {
       sku: 17605827,
       warranty: 'lifetime',
-      items: [11,2,3]
-
     },
     {
       sku: 17605421,
       warranty: 'lifetime',
-      items: [11,23,3]
     },
     {
       sku: 30122459,
       warranty: 3,
-      items: [113,2,3]
     },
     {
       sku: 30047575,
@@ -54,27 +50,15 @@ let PRODUCTS = [
     
     for (let i = 0; i < PRODUCTS.length; i++) {
 
-         const product = PRODUCTS[i].sku; 
-         console.log(product)
-         for (let i = 0; i < ITEMS_WITH_WARRANTY.length; i++) {
-             const item = ITEMS_WITH_WARRANTY[i].sku;
-             console.log(item)
-             if(product === item){
-                 
-                return {...PRODUCTS[i], ...ITEMS_WITH_WARRANTY[i]}
-            }  
-            }
-     /*    console.log(product)
-        if(product === sku){
+         const single = PRODUCTS[i].sku; 
+        console.log(single)
+        if(single === sku){
             return PRODUCTS[i].currency
-        } */
+        }
       
     }
   }
   console.log(findSKU(17605421))
 
 const getLiftimeWarranty =  ITEMS_WITH_WARRANTY.filter(x => x.warranty === 'lifetime')
-const getLiftimeWarrantyMap =  ITEMS_WITH_WARRANTY.map(x => x)
-
-
-console.log(getLiftimeWarrantyMap)
+console.log(getLiftimeWarranty)
